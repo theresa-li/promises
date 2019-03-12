481ea11dbd0c2d68f4435e67e7b79df1f4ab778b
theresa-li <tli2@cca.edu>
2019-03-12 15:24:57 -0700

    work on promisification and promiseConstructor for Promises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -8,23 +8,23 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath, callback) {
   fs.readFile(filePath, 'utf8', (err, data) => {
-    if(err) {
+    if (err) {
       callback(err, data);
     } else {
       callback(null, data.split('\n')[0]);
     }
-  })
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
   request.get(url, (err, data) => {
-    if(err) {
+    if (err) {
       callback(err);
     } else {
       callback(null, data.statusCode);
     }
-  })
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
